be049ec17c0699ce94436646680bcb5e6f63404c
maher <gomemahero.com>
2022-05-16 00:27:29 +0300

    color images is null create  product is work

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import axios from "axios";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 import { useState, useEffect, useRef } from "react";
-import { Select, Input,Button } from "antd";
+import { Select, Input, Button } from "antd";
 const { Option } = Select;
 
 const Product = () => {
-
   const apiURL = "http://localhost:5000/api/product/add-product";
 
-// fetch from redux category
-  const {allcategories} = useSelector(state => state.category);
+  // fetch from redux category
+  const { allcategories } = useSelector((state) => state.category);
 
   const hiddenFileInput = useRef(null);
-const hiddencolorFileInput = useRef(null);
+  const hiddencolorFileInput = useRef(null);
 
   const [name, setName] = useState("");
-  const [price, setPrice] = useState([]);
+  const [discount, setDiscount] = useState(0);
+  const [price, setPrice] = useState(0);
   const [media, setMedia] = useState(null);
   const [resImage, setResImage] = useState([]);
+
+  const [showmainImages, setShowmainImages] = useState([]);
+  console.log("showmainImages--->", showmainImages);
+  const [showcolorImages, setShowcolorImages] = useState([]);
 
   const [files, setFiles] = useState("");
   const [colorsfiles, setcolorsFiles] = useState(""); // handle colorsimages files change
@@ -28,7 +32,7 @@ const hiddencolorFileInput = useRef(null);
   const [tags, setTags] = useState([]);
   const [category, setCategory] = useState([]);
 
-  const [oldprice, setOldprice] = useState();
+  const [oldprice, setOldprice] = useState(0);
 
   // make mathrandom numbe
 
@@ -42,6 +46,14 @@ const hiddencolorFileInput = useRef(null);
     "grey",
     "green",
     "white",
+    "yellow",
+    "brown",
+    "beige",
+    "purple",
+    "pink",
+    "silver",
+    "gold",
+
   ];
 
   const children = [];
@@ -67,6 +79,7 @@ const hiddencolorFileInput = useRef(null);
   // ---tags----
 
   const tagsArray1 = [
+
     "Clocks",
     "Minimalism",
     " Lighting",
@@ -113,131 +126,118 @@ const hiddencolorFileInput = useRef(null);
     console.log("tags result-->", tags);
   }
 
-
-
-  const handleClickUplaodicon = event => {
+  const handleClickUplaodicon = (event) => {
     hiddenFileInput.current.click();
   };
-  
-  const handleClickUplaodiconcolors = event => {
+
+  const handleClickUplaodiconcolors = (event) => {
     hiddencolorFileInput.current.click();
   };
 
-
-
-const handleChangeImages = event => {
+  const handleChangeImages = (event) => {
     const fileUploaded = event.target.files;
     console.log("fileUploaded", fileUploaded);
     setFiles(fileUploaded);
     console.log("files-main-images", files);
   };
 
-
-
-
   // handle colorsimages files change
 
-  const handlecolorsChangeImages = event => {
+  const handlecolorsChangeImages = (event) => {
     const colorsfileUploaded = event.target.files;
     setcolorsFiles(colorsfileUploaded);
     console.log("colorsfiles upload colors---->", files);
   };
 
+  // send data to server
+
+  const handleclick = async (event) => {
+    event.preventDefault();
+    try {
+      const listimages = await Promise.all(
+        // listimages is--> array of  objects --> object mainimgs = {secure_url:secure_url,public_id:public_id};
+        Object.values(files).map(async (file) => {
+          const data = new FormData();
+          data.append("file", file);
+          data.append("upload_preset", "mystory123");
+          data.append("folder", "/products-main");
+          const uploadRes1 = await axios.post(
+            "https://api.cloudinary.com/v1_1/maher9911133/image/upload",
+            data
+          );
+
+          const { url, secure_url, public_id } = uploadRes1.data;
+
+          setShowmainImages(url)
+          console.log("showimages------>", showmainImages);
+
+       //   console.log("uploadRes mainnnnnnn", uploadRes1);
 
 
-// send data to server
 
-const handleclick = async event => {
-  event.preventDefault();
-try {
-
-  const listimages = await Promise.all(  
-    // listimages is--> array of  objects --> object mainimgs = {secure_url:secure_url,public_id:public_id};
-    Object.values(files).map(async (file) => {
-      const data = new FormData();
-      data.append("file", file);
-      data.append("upload_preset", "mystory123");
-      data.append("folder", "/products-main");
-      const uploadRes1 = await axios.post(
-        "https://api.cloudinary.com/v1_1/maher9911133/image/upload",
-        data
+          let mainimgs = {};
+          return (mainimgs = { secure_url: secure_url, public_id: public_id });
+        })
       );
 
-      const { url,secure_url,public_id } = uploadRes1.data;
-      
-    console.log("uploadRes mainnnnnnn", uploadRes1);
-
-      let mainimgs ={}
-      return  mainimgs = {secure_url:secure_url,public_id:public_id};
-    })
-  );
 
 
 
-// ---images colors upload to cloudinary----
+      // ---images colors upload to cloudinary----
 
-const colorslist = await Promise.all(  
-  // colorslist is--> array of  objects --> object mainimgs = {secure_url:secure_url,public_id:public_id};
-  Object.values(colorsfiles).map(async (file) => {
-    const datacolor = new FormData();
-    datacolor.append("file", file);
-    datacolor.append("upload_preset", "mystory123");
-    datacolor.append("folder", "/products-colors");
-    const uploadRes2 = await axios.post(
-      "https://api.cloudinary.com/v1_1/maher9911133/image/upload",
-      datacolor
-    );
+// if  (colorsfiles.length !== null) {
 
-    console.log("uploadRes colorssssssssssssssssss", uploadRes2);
+      const colorslist = await Promise.all(
+        // colorslist is--> array of  objects --> object mainimgs = {secure_url:secure_url,public_id:public_id};
+        Object.values(colorsfiles).map(async (file) => {
+          const datacolor = new FormData();
+          datacolor.append("file", file);
+          datacolor.append("upload_preset", "mystory123");
+          datacolor.append("folder", "/products-colors");
+          const uploadRes2 = await axios.post(
+            "https://api.cloudinary.com/v1_1/maher9911133/image/upload",
+            datacolor
+          );
 
-    const { url,secure_url,public_id } = uploadRes2.data;
+          console.log("uploadRes colorssssssssssssssssss", uploadRes2);
 
-     let colsmgs ={}
-    return   colsmgs = {secure_url:secure_url,public_id:public_id};
-  })
-);
+          const { url, secure_url, public_id } = uploadRes2.data;
 
+          let colsmgs = {};
+          return (colsmgs = { secure_url: secure_url, public_id: public_id });
+        })
+      );
 
+    
 
+      //--------------------------------------------------
 
-//--------------------------------------------------
+      const newproduct = {
+        price,
+        name,
+        discount,
+        colorimages: colorslist.length > 0 ? colorslist : null,
+        category: category,
+        oldprice,
+        tags: tags[0],
+        colors: colors[0],
+        images: listimages, // array of urls of images
+      };
+      console.log("new product--->", newproduct);
 
-  const newproduct = {
-    price,
-    name,
-     colorimages:colorslist,
-    category:category,
-     oldprice,
-     tags:tags[0],
-    colors: colors[0],
-   images: listimages, // array of urls of images
- };
- console.log("new product--->", newproduct);
-
- await axios.post(apiURL, newproduct).then((res) => {
-  console.log("respons name from post product-->", res);
-});
-
-
-
-}
-
-catch (error) {
-  console.log(error);
-}
-
-
-
-
-}
-
-
+      await axios.post(apiURL, newproduct).then((res) => {
+        console.log("respons name from post product-->", res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="container pb-[50px] mb-55px text-center">
       <div>
         <h1 className="text-xl w-[300px] mx-auto bg-blue-300 p-[6px] rounded-lg">
-          Product Create  {category}
+          Product Create   {showmainImages.length}
         </h1>
       </div>
 
@@ -257,62 +257,43 @@ catch (error) {
 
             {/* ----------------- */}
 
+            {/* -=categories-- */}
 
-{/* -=categories-- */}
+            <div>
+              <div className=" w-[300px] p-[20px] p1 mt-[20px] mb-4
+              
+              ">
+                {/* make caetegories in select inputs  */}
+                {category}
+                <select
+                className="w-[100%]    border border-solid border-green-300
+                p1
+                rounded
+                transition
+                ease-in-out   focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  onChange={(e) => setCategory(e.target.value)}
+                  value={category.name}
+                >
+                  {allcategories.map((category) => (
+                    <option value={category._id}>{category.name}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
 
 
+{/* ---discount-- */}
 
 <div>
+              <h1 className="font-bold p1"> product discount</h1>
 
-<div>
-
-
-{/* make caetegories in select inputs  */}
-{category}
-<select
-onChange={(e) => setCategory(e.target.value)}
-value={category.name}
-
-
->
-
-
-{   allcategories.map((category) => (
-  
- 
-  
-    <option value={category._id}>{category.name}</option>
-  
-
-    
-  
-  
-  ))}
-  
-
-
-
-
-</select>
-
-
-
-
-
-
-
-
-</div>
-
-
-
-</div>
-
-
-
-
-
-
+              <Input
+                type="number"
+                id="numberdiscount"
+                onChange={(e) => setFiles(e.target.value)}
+                placeholder="Product-discount"
+              />
+            </div>
 
 
 
@@ -388,54 +369,91 @@ value={category.name}
               </Select>
             </div>
 
-
-{/* --upload images-- */}
-<div className="p-[44px] ">
-
-
-<>
-      <Button className="w-[189px] p-[12px]" danger onClick={handleClickUplaodicon}>
-        Upload Main Images
-      </Button>
-      <input type="file"
-      multiple
-             ref={hiddenFileInput}
-             onChange={handleChangeImages}
-             style={{display:'none'}} 
-      /> 
-    </>
-    </div>
-
-
-    <>
-      <Button className="w-[189px] mb-[44px]"  danger onClick={handleClickUplaodiconcolors}>
-        Upload Colors Images
-      </Button>
-      <input type="file"
-      multiple
-             ref={hiddencolorFileInput}
-             onChange={handlecolorsChangeImages}
-             style={{display:'none'}} 
-      /> 
-    </>
+            {/* --upload images-- */}
+            <div className="p-[44px] ">
+              <>
+                <Button
+                  className="w-[189px] p-[12px]"
+                  danger
+                  onClick={handleClickUplaodicon}
+                >
+                  Upload Main Images
+                </Button>
+                <input
+                  type="file"
+                  multiple
+                  ref={hiddenFileInput}
+                  onChange={handleChangeImages}
+                  style={{ display: "none" }}
+                />
+              </>
+            </div>
 
 
-
-{/* ------ send product ---- */}
+{/* ---------main images show---- */}
 
 
 <div>
 
+<div>
 
-<Button  className="w-[190px] bg-green-200"
-onClick={ handleclick}
->CreateProduct</Button>
+
+{ showmainImages && showcolorImages.length > 0 &&  showmainImages.map((image, index) => (
+  
+
+<div>
+
+<img src={image} alt="" />
+
+</div>
+
+))}
+
+
+
+
+
 
 
 </div>
 
 
 
+
+
+
+
+
+</div>
+
+
+
+
+
+            <>
+              <Button
+                className="w-[189px] mb-[44px]"
+                danger
+                onClick={handleClickUplaodiconcolors}
+              >
+                Upload Colors Images
+              </Button>
+              <input
+                type="file"
+                multiple
+                ref={hiddencolorFileInput}
+                onChange={handlecolorsChangeImages}
+                style={{ display: "none" }}
+              />
+            </>
+
+            {/* ------ send product ---- */}
+
+            <div>
+              <Button className="w-[190px] bg-green-200" onClick={handleclick}>
+                CreateProduct
+              </Button>
+            </div>
           </div>
         </div>
       </div>
