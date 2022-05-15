12690eab44befacd9a246c98d6df97f8fde76279
maher <gomemahero.com>
2022-05-15 18:06:55 +0300

    categoies make and send to database is superrr

---
+++ src/pages/product/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div>
+      <div>
+        <h1>Create Product</h1>
+
+        <div>
+             
+
+
+
+
+
+             
+              <div>
+
+              </div>
+        </div>
+
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
